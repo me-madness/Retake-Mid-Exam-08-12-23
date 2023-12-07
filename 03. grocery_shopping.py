1e298cd9423bdbbd1ecd729012a5ff0aba72a6e5
me_madness <p_rangel@abv.bg>
2023-12-07 14:39:57 +0100

    done

--- 03. grocery_shopping.py
+++ 03. grocery_shopping.py
@@ -1,6 +1,8 @@
 list_of_product = input().split("|")
 command = input().split("%")
 new_list_of_products = []
+count_product = 1
+
 while command[0] != "Shop!":
     
     if command[0] == "Important":
@@ -9,8 +11,7 @@ while command[0] != "Shop!":
             list_of_product.insert(0,product)
         else:
             list_of_product.remove(product)
-            list_of_product.insert(0,product)
-            print(list_of_product)    
+            list_of_product.insert(0,product)   
     elif command[0] == "Add":
         if product not in list_of_product:
             list_of_product.append(product)
@@ -25,7 +26,8 @@ while command[0] != "Shop!":
             if product_two not in list_of_product:
                 print(f"Product {product_two} missing!")
             else:
-                pass    
+                a, b = list_of_product.index(product_one), list_of_product.index(product_two)
+                list_of_product[b], list_of_product[a] = list_of_product[a], list_of_product[b]      
     elif command[0] == "Remove":
         ex_product = command[1]
         if ex_product not in list_of_product:
@@ -36,6 +38,10 @@ while command[0] != "Shop!":
         new_list_of_products.reverse(list_of_product)
         list_of_product = new_list_of_products
     command = input().split("%")
+    
+for product in list_of_product:
+    print(f"{count_product}.{product}") 
+    count_product += 1   
     
     
 # eggs|milk|bread|fish    
