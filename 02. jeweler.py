1d79e9b2ca63cf4e04d98fa2280ba68f2d3adcf2
me_madness <p_rangel@abv.bg>
2023-12-07 16:35:59 +0100

    02 begin

---
+++ 02. jeweler.py
@@ -0,0 +1,16 @@
+white_gold = input().split(" ")
+yellow_gold = input().split(" ")
+
+print(white_gold)
+print(yellow_gold)
+
+# Input 1
+
+# 2 7 8 5 1 6 1 7 5
+# 8 3 2 7 9 4 9 2 3
+
+
+# Input 2
+
+# 5 3 2 2 4
+# 5 5 8 2 6
