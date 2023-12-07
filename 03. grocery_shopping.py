060ebd04e3ed923cbc08403e9dfab452e017ed0a
me_madness <p_rangel@abv.bg>
2023-12-07 13:55:22 +0100

    begin of retake mid exm

---
+++ 03. grocery_shopping.py
@@ -0,0 +1,19 @@
+list_of_product = input().split("|")
+command = input()
+
+while command != "Shop":
+    command = input().split("%")
+    
+    if command[0] == "Important":
+        pass
+    elif command[0] == "Add":
+        pass
+    elif command[0] == "Swap":
+        pass
+    elif command[0] == "Remove":
+        pass
+    elif command[0] == "Reversed":
+        pass
+    
+    
+    
